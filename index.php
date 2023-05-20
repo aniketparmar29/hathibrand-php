d06d6d2f25e98e3aee5c85ee2623e0c6c6d618c4
aniketparmar29 <aniketparmar068@gmail.com>
2023-05-20 10:27:36 +0530

    ASD

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    include('.\conn\dbconnection.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="./assets/Logo/Favicon.ico" type="image/x-icon">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hathibrand</title>
+    <script src="./styles/tailwind.css"></script>
+</head>
+
+<body>
+    <nav>
+        <img src="./assets/Logo/Favicon.ico" alt="">
+        <ul class="">
+            <li>Home</li>
+            <li>Categorise</li>
+            <li>Cart</li>
+            <li>Login</li>
+        </ul>
+    </nav>
+</body>
+
+</html>
