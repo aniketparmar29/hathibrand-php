5b68b722b6d5f39e016d137be8e7b3b92722602c
aniketparmar29 <aniketparmar068@gmail.com>
2023-05-26 17:18:19 +0530

    ASD

--- Products.php
+++ Products.php
@@ -3,23 +3,23 @@
 
     // Get sorting and filtering options from the query parameters
     $sort = isset($_GET['sort']) ? $_GET['sort'] : ''; // Sorting option: price_htl, price_lth, weight_htl, weight_lth
-    $category = isset($_GET['category']) ? $_GET['category'] : ''; // Filtering option: category
+    $product_category = isset($_GET['product_category']) ? $_GET['product_category'] : ''; // Filtering option: product_category
 
     // Construct the SQL query based on the sorting and filtering options
     $sql = "SELECT * FROM categories";
-    if ($category !== '') {
-        $sql .= " WHERE category = '$category'";
+    if ($product_category !== '') {
+        $sql .= " WHERE product_category = '$product_category'";
     }
 
     $order = '';
     if ($sort === 'price_htl') {
-        $order = 'ORDER BY price DESC';
+        $order = 'ORDER BY product_price DESC';
     } elseif ($sort === 'price_lth') {
-        $order = 'ORDER BY price ASC';
+        $order = 'ORDER BY product_price ASC';
     } elseif ($sort === 'weight_htl') {
-        $order = 'ORDER BY weight DESC';
+        $order = 'ORDER BY product_weight DESC';
     } elseif ($sort === 'weight_lth') {
-        $order = 'ORDER BY weight ASC';
+        $order = 'ORDER BY product_weight ASC';
     }
 
     if ($order !== '') {
@@ -33,6 +33,8 @@
 <html>
 <head>
     <title>Products Page</title>
+    <link rel="shortcut icon" href="./assets/Logo/Favicon.ico" type="image/x-icon">
+
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
@@ -53,42 +55,117 @@
                 </select>
             </div>
             <div>
-                <label for="category">Filter by category:</label>
-                <select id="category" name="category" onchange="location = this.value;">
+                <label for="product_category">Filter by Category:</label>
+                <select id="product_category" name="product_category" onchange="location = this.value;">
                     <option value="">All</option>
-                    <option value="?category=Agarbatti">Agarbatti</option>
-                    <option value="?category=Cosmetic">Cosmetics</option>
+                    <option value="?product_category=Agarbatti">Agarbatti</option>
+                    <option value="?product_category=Cosmetic">Cosmetics</option>
                 </select>
             </div>
         </div>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            <?php
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    $productName = $row["name"];
-                    $productImage = $row["image"];
-                    $productPrice = $row["price"];
-                    if ($row["stock"] > 0) {
-                        ?>
-                        <div class="bg-white rounded-lg shadow-md p-4">
-                            <img src="<?php echo $productImage; ?>" alt="<?php echo $productName; ?>"
-                                 class="object-contain h-48 w-full mb-4">
-                            <h2 class="text-lg font-bold mb-2"><?php echo $productName; ?></h2>
-                            <p class="text-gray-600">$<?php echo $productPrice; ?></p>
-                        </div>
-                        <?php
-                    }
-                }
-            } else {
-                echo "No products found.";
-            }
+        <div class="container mx-auto grid gap-8 grid-cols-2 lg:grid-cols-4">
+    <?php
 
-            $conn->close();
-            ?>
+
+ 
+while ($row = mysqli_fetch_assoc($result)) {
+    $productID = $row['id'];
+    $productName = $row['product_name'];
+    $productImage = explode(',', $row['product_images']);
+    $productStock = $row['product_stock'];
+    $productWeight = $row['product_weight'];
+    $productDesc = $row['product_desc'];
+    $product_category = $row['product_category'];
+    $productPrice = $row['product_price'];
+
+    // Check if the product is already in the wishlist
+    
+    ?>
+
+<div class="bg-white shadow-lg rounded-lg p-6 relative overflow-hidden">
+  <?php if (!empty($productImage[0])) { ?>
+    <img src="./Admin/<?php echo $productImage[0]; ?>" alt="Product Image">
+  <?php } ?>
+  <div class="mt-4">
+    <h2 class="text-xl font-semibold"><?php echo $productName; ?></h2>
+    <p class="text-lg mt-2">Weight:<?php echo $productWeight; ?></p>
+    <div class="flex justify-between items-center mt-4">
+      <span class="text-gray-600">
+        <i class="fas fa-rupee-sign"></i> <?php echo $productPrice; ?>
+      </span>
+      <div class="flex items-center space-x-2 absolute top-2 right-0">
+          <button class="text-red-500  transition-colors duration-300"
+            title="Add to Wishlist"
+            onclick="addToWishlist(<?php echo $productID; ?>, '<?php echo $productName; ?>', '<?php echo $productImage[0]; ?>', '<?php echo $productWeight; ?>', '<?php echo $productPrice; ?>')">
+            <i class="far fa-heart text-2xl hover:fa"></i>
+          </button>
+        
         </div>
+        <button class="text-blue-500 hover:text-blue-600 transition-colors duration-300"
+          title="Add to Cart">
+          <i class="fas fa-shopping-cart text-2xl "></i>
+        </button>
+    </div>
+  </div>
+</div>
+
+
+<?php
+}
+
+mysqli_free_result($result);
+mysqli_close($conn);
+?>
+</div>
     </div>
     <?php include './components/Footer.php'?>
 
 </body>
 </html>
+<script>
+    function addToWishlist(productID, productName, productImage, productWeight, productPrice) {
+  // Get the product details
+  var productDetails = {
+    id: productID,
+    name: productName,
+    image: productImage,
+    weight: productWeight,
+    price: productPrice
+  };
+
+  // Check if localStorage is available
+  if (typeof (Storage) !== "undefined") {
+    // Retrieve existing wishlist items from localStorage
+    var wishlistItems = localStorage.getItem("wishlist");
+    var wishlist = [];
+
+    if (wishlistItems !== null) {
+      wishlist = JSON.parse(wishlistItems);
+    }
+
+    // Check if the product is already in the wishlist
+    var isProductInWishlist = wishlist.some(function(item) {
+      return item.id === productID;
+    });
+
+    if (isProductInWishlist) {
+      // Product already in wishlist, display message to the user
+      alert("Product is already in the wishlist!");
+    } else {
+      // Add the product to the wishlist
+      wishlist.push(productDetails);
+
+      // Save the updated wishlist back to localStorage
+      localStorage.setItem("wishlist", JSON.stringify(wishlist));
+
+      // Provide feedback to the user
+      alert("Product added to wishlist!");
+    }
+  } else {
+    // localStorage is not available
+    alert("Your browser does not support localStorage")
+  }
+}
+
+</script>
--- components/Navbar.php
+++ components/Navbar.php
@@ -10,25 +10,26 @@
 </head>
 <body>
     <nav class="flex z-50 flex-row justify-between items-center lg:sticky top-0 lg:px-5 px-2 bg-gray-200">
-    <a href="./index.php"><img class="w-24" src="./assets/Logo/Favicon.ico" alt=""></a>
+        <a href="./index.php"><img class="w-24" src="./assets/Logo/Favicon.ico" alt=""></a>
         <div class="relative">
-            <input type="text" name="search" id="search" class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 pl-10" placeholder="Search...">
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <i class="fa-solid fa-magnifying-glass"></i>
-            </div>
-        </div>
+    <input type="text" name="search" id="search" class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 pl-10" placeholder="Search...">
+    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+        <i class="fa-solid fa-magnifying-glass" id="searchIcon"></i>
+    </div>
+</div>
 
         <!-- Desktop navigation menu -->
         <ul class="desktop-menu text-red-500 hidden md:flex lg:flex md:text-lg flex-row gap-x-5 text-xl">
             <li class="hover:underline"><a href="./index.php">Home</a></li>
             <li class="hover:underline"><a href="./Products.php">Categories</a></li>
-            <li class="hover:underline"><a href="">Cart</a></li>
+            <li class="hover:underline"><a href="cart.php"><i class="fa fa-shopping-cart"></i></a></li>
+            <li class="hover:underline"><a href="wishlist.php"><i class="fa fa-heart"></i></a></li>
             <li class="hover:underline"><a href="./Login.php">Login</a></li>
         </ul>
-
-        
     </nav>
 
+    
+<div id="search-results"></div>
     <section class="fixed mobile-menu block lg:hidden md:hidden bottom-0 inset-x-0 z-50 shadow-lg bg-gray-700 dark:bg-dark backdrop-blur-lg bg-opacity-30 dark:bg-opacity-30 border-t-2 border-royal/20">
         <div id="tabs" class="flex justify-between">
             <a href="/" class="w-full focus:text-royal hover:text-royal justify-center inline-block text-center pt-2 pb-1 hover:bg-white">
@@ -44,14 +45,14 @@
 >
                         <span class="tab block text-xs font-extrabold text-yellow-600">Categories</span>
                       </a>
-                      <a href="/cart" class="w-full focus:text-royal hover:text-royal justify-center inline-block text-center pt-2 pb-1  hover:bg-white">
+                      <a href="cart.php" class="w-full focus:text-royal hover:text-royal justify-center inline-block text-center pt-2 pb-1  hover:bg-white">
                         <div class="h-6 w-6 inline-block mb-1">
                             <i class="fa-solid text-red-500 fa-cart-shopping"></i>
                         </div>
                         <span class="tab block text-xs font-extrabold text-yellow-600">Cart <span>0</span></span>
                       </a>
                        
-                        <a href="/login" class="w-full focus:text-royal hover:text-royal justify-center inline-block text-center pt-2 pb-1 hover:bg-white">
+                        <a href="login.php" class="w-full focus:text-royal hover:text-royal justify-center inline-block text-center pt-2 pb-1 hover:bg-white">
                             <div class="h-6 w-6 inline-block mb-1">
                                 <i class="fa-solid text-red-500 fa-user"></i>
                             </div>
@@ -60,8 +61,49 @@
                     </div>
                   </section>
                   
+                  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                  <script>
+var timer = null;
+var searchInput = document.getElementById('search');
+var searchResults = document.getElementById('search-results');
 
-<script>
+searchInput.addEventListener('keyup', function() {
+  clearTimeout(timer);
+  timer = setTimeout(makeRequest, 300);
+});
+
+searchInput.addEventListener('input', function() {
+  clearTimeout(timer);
+  timer = setTimeout(makeRequest, 300);
+});
+
+function makeRequest() {
+  var query = searchInput.value.trim();
+  if (query !== '') {
+    var request = new XMLHttpRequest();
+    request.open('POST', './search.php', true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.onreadystatechange = function() {
+      if (request.readyState === 4 && request.status === 200) {
+        var results = JSON.parse(request.responseText);
+        var output = '';
+        for (var i = 0; i < results.length; i++) {
+          output += '<div>';
+          output += '<h3>' + results[i].name + '</h3>';
+          output += '<p>' + results[i].description + '</p>';
+          output += '</div>';
+        }
+        searchResults.innerHTML = output;
+      }
+    };
+    request.send('query=' + encodeURIComponent(query.toLowerCase()));
+  } else {
+    searchResults.innerHTML = '';
+  }
+}
+
+
+
 </script>
 
 </body>
