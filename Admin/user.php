5a726e7dea83f84a943b37f05868b5ebaf3d1fc6
aniketparmar29 <aniketparmar068@gmail.com>
2023-06-11 09:28:38 +0530

    AD

--- Admin/user.php
+++ Admin/user.php
@@ -8,6 +8,8 @@ include '../dbconnection.php'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./assets/Logo/Favicon.ico" type="image/x-icon">
+
     <title>Document</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
